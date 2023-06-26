4f15af12219e0426343fbeb339262a63a0c4b3a2
Patryk Waluś <patrykwal12@gmail.com>
2023-06-26 23:31:25 +0200

    Add Codexpect_UnitTests module

---
+++ app/code/Codexpect/UnitTests/Test/Unit/ProductTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Codexpect\UnitTests\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(false);
+    }
+}
---
+++ app/code/Codexpect/UnitTests/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Codexpect_UnitTests', __DIR__);

3d5dfd3776cb523bf23f088d8cd0005f880581e0
Patryk Waluś <patrykwal12@gmail.com>
2023-07-31 11:01:50 +0200

    Db schema

---
+++ app/code/Codexpect/IntegrationTests/Test/Integration/Sales/OrderAuthTest.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Codexpect\IntegrationTests\Test\Integration\Sales;
+
+use PHPUnit\Framework\TestCase;
+
+class OrderAuthTest extends TestCase
+{
+
+    public function testCancel_OrderCanceled_StatisticsSaved()
+    {
+
+    }
+
+    public function setUp(): void
+    {
+
+    }
+
+}

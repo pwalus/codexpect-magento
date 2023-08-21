d556f7d34009cbef02e479b8fa126df2c5a2a662
Patryk Waluś <patrykwal12@gmail.com>
2023-08-21 13:37:57 +0200

    Add integration _files

--- dev/tests/integration/etc/ci.workflow.install-config-mysql.php
+++ dev/tests/integration/etc/ci.workflow.install-config-mysql.php
@@ -5,22 +5,21 @@
  */
 
 return [
-    'db-host' => '127.0.0.1',
+    'db-host' => 'mariadb',
     'db-user' => 'root',
     'db-password' => 'password',
     'db-name' => 'magento_integration_tests',
     'backend-frontname' => 'backend',
     'search-engine' => 'opensearch',
-    'elasticsearch-host' => '127.0.0.1',
-    'elasticsearch-port' => 9200,
+    'opensearch-host' => 'opensearch',
+    'opensearch-port' => '9200',
     'admin-user' => \Magento\TestFramework\Bootstrap::ADMIN_NAME,
     'admin-password' => \Magento\TestFramework\Bootstrap::ADMIN_PASSWORD,
     'admin-email' => \Magento\TestFramework\Bootstrap::ADMIN_EMAIL,
     'admin-firstname' => \Magento\TestFramework\Bootstrap::ADMIN_FIRSTNAME,
     'admin-lastname' => \Magento\TestFramework\Bootstrap::ADMIN_LASTNAME,
-    'amqp-host' => '127.0.0.1',
+    'amqp-host' => 'rabbitmq',
     'amqp-port' => '5672',
     'amqp-user' => 'guest',
     'amqp-password' => 'guest',
-    'disable-modules'   => join(',', []),
 ];
